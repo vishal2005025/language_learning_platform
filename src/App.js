78d806fdbf7c55afe49471f6d7f9804338666263
vishal2005025 <vishalmalyan2005@gmail.com>
2023-11-08 20:52:00 +0530

    login page hold and header section complete with some images add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
-import LoginSignup from './components/LoginSignup/LoginSignup';
+import Header from './components/common/heading/Header';
+
+/*import LoginSignup from './components/LoginSignup/LoginSignup';*/
+/*import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
+/*<Route path='/' element= {<LoginSignup/>} />*/
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div >
-     <LoginSignup/>
-    </div>
+<>
+<Router>
+
+<Header/>
+<Routes>
+
+<Route path='/' element={<Home/>}/>
+
+
+</Routes>      
+</Router>
+
+
+
+</>
+
+
+/*<BrowserRouter>
+      <Routes>
+    
+    
+    
+   
+      </Routes>
+ 
+    </BrowserRouter>*/
+  
   );
 }
 
--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -6,6 +6,7 @@ import './LoginSignup.css'
 import user_icon from '../assets/person.png'
 import email_icon from '../assets/email.png'
 import password_icon from '../assets/password.png'
+import { Link } from 'react-router-dom'
 
 
 const LoginSignup = () => {
@@ -39,7 +40,7 @@ const [action,setAction] = useState("Login");
       {action==="Sign Up"?<div></div>:<div className="forget-password">Lost Password? <span>Click Here!</span></div>}
       
       <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}> Sign Up</div>
         <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
