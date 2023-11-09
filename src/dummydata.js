396641ca812d61bb99ed6e44a08e2ad2cac491d6
vishal2005025 <vishalmalyan2005@gmail.com>
2023-11-09 23:23:07 +0530

    updated name

--- src/dummydata.js
+++ src/dummydata.js
@@ -3,19 +3,19 @@ export const homeAbout = [
     id: 1,
     cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
     title: "Online Courses",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    desc: " Sure thing! Online courses are a fantastic way to learn and acquire new skills without the constraints of a traditional classroom.",
   },
   {
     id: 1,
     cover: "https://img.icons8.com/ios/80/000000/diploma.png",
     title: "Earn A Certificates",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    desc: " Earning a certificate through online courses can be a great way to showcase your skills and knowledge in a particular field. ",
   },
   {
     id: 1,
     cover: "https://img.icons8.com/ios/80/000000/athlete.png",
     title: "Learn with Expert",
-    desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+    desc: "Learning from experts can provide invaluable insights and accelerate your understanding of a subject.It adds a personal touch to your learning journey. ",
   },
 ]
 export const awrapper = [
@@ -49,12 +49,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by John Smith",
+        name: "by vishal malyan",
         totalTime: "50 lectures (190 hrs)",
       },
     ],
-    priceAll: "$100 All Course",
-    pricePer: "$15 per month",
+    priceAll: "Rs.1000 All Course",
+    pricePer: "Rs.150 per month",
   },
   {
     id: 2,
@@ -63,12 +63,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Ram Gurung",
+        name: "by vishal malyan",
         totalTime: "30 lectures (125 hrs)",
       },
     ],
-    priceAll: "$200 All Course",
-    pricePer: "$25 per month",
+    priceAll: "Rs.2000 All Course",
+    pricePer: "Rs.250 per month",
   },
   {
     id: 3,
@@ -77,12 +77,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Saroj Nepal",
+        name: "by vishal malyan",
         totalTime: "50 lectures (50 hrs)",
       },
     ],
-    priceAll: "$50 All Course",
-    pricePer: "$10 per month",
+    priceAll: "Rs.5000 All Course",
+    pricePer: "Rs.400 per month",
   },
   {
     id: 4,
@@ -91,12 +91,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Prabin Thapa Mager",
+        name: "by vishal malyan",
         totalTime: "20 lectures (20 hrs)",
       },
     ],
-    priceAll: "$30 All Course",
-    pricePer: "$13 per month",
+    priceAll: "Rs.3000 All Course",
+    pricePer: "Rs.300 per month",
   },
   {
     id: 5,
@@ -105,12 +105,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Sunil Bishowkaram",
+        name: "by vishal malyan",
         totalTime: "100 lectures (150 hrs)",
       },
     ],
-    priceAll: "$300 All Course",
-    pricePer: "$30 per month",
+    priceAll: "Rs.3000 All Course",
+    pricePer: "Rs.300 per month",
   },
   {
     id: 6,
@@ -119,12 +119,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Sunil Bishowkaram",
+        name: "by vishal malyan",
         totalTime: "200 lectures (300 hrs)",
       },
     ],
-    priceAll: "$500 All Course",
-    pricePer: "$80 per month",
+    priceAll: "Rs.4000 All Course",
+    pricePer: "Rs.400 per month",
   },
   {
     id: 7,
@@ -133,12 +133,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Price",
+        name: "by vishal malyan",
         totalTime: "20 lectures (50 hrs)",
       },
     ],
-    priceAll: "$20 All Course",
-    pricePer: "$12 per month",
+    priceAll: "Rs.2000 All Course",
+    pricePer: "Rs.200 per month",
   },
   {
     id: 8,
@@ -147,12 +147,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Petter",
+        name: "by vishal malyan",
         totalTime: "80 lectures (200 hrs)",
       },
     ],
-    priceAll: "$200 All Course",
-    pricePer: "$40 per month",
+    priceAll: "Rs.2000 All Course",
+    pricePer: "Rs.300 per month",
   },
   {
     id: 9,
@@ -161,12 +161,12 @@ export const coursesCard = [
     courTeacher: [
       {
         dcover: "./images/back.webp",
-        name: "by Petter",
+        name: "by vishal malyan",
         totalTime: "50 lectures (50 hrs)",
       },
     ],
-    priceAll: "$20 All Course",
-    pricePer: "$10 per month",
+    priceAll: "Rs.1500 All Course",
+    pricePer: "Rs.200 per month",
   },
 ]
 export const online = [
@@ -246,42 +246,42 @@ export const online = [
 export const team = [
   {
     cover: "./images/team/t1.webp",
-    name: "Ph.D Adrian Molises",
+    name: "Ph.D Vishal Malyan",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t2.webp",
-    name: "Ph.D Arthur ",
+    name: "Ph.D Abhi Verma ",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t3.webp",
-    name: "Ph.D Anna Hanzen",
+    name: "Ph.D Samaviah",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t4.webp",
-    name: "Ph.D Brian Wooden",
+    name: "Ph.D Tarun Lalwani",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t5.webp",
-    name: "Ph.D Adrian Molises",
+    name: "Ph.D Shorya",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t6.webp",
-    name: "Ph.D Arthur ",
+    name: "Ph.D Shreyansh Sachan ",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t7.webp",
-    name: "Ph.D Anna Hanzen",
+    name: "Ph.D Nandini Sharma",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
   {
     cover: "./images/team/t8.webp",
-    name: "Ph.D Brian Wooden",
+    name: "Ph.D Abhishek",
     work: "DEVELOPER AND LEAD INSTRUCTOR",
   },
 ]
