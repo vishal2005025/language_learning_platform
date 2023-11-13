89c7fb1785d419725ac641f348cd7fe783143777
vishal2005025 <vishalmalyan2005@gmail.com>
2023-11-13 09:30:59 +0530

    update conflict errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginSignup from './components/LoginSignup/LoginSignup';
+
 
 function App() {
   return (
     <div >
-     <LoginSignup/>
+    
     </div>
   );
 }
