dcc68da1b1d6a5f01dd6232d53e8fcbdca08609c
abhishek kumar <ruhelabhishek@gmail.com>
2023-11-23 15:44:15 +0530

    addingContent

--- backend/LanguageProject/Languageapp/models.py
+++ backend/LanguageProject/Languageapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class student(models.Model):
-    UserNames = models.CharField(max_length=100, default='student')
-    emails = models.CharField(max_length=100, default='abcd') 
-    password =models.CharField(max_length=50 , default='9837') 
+    UserNames = models.CharField(max_length=100, default='null')
+    emails = models.CharField(max_length=100, default='null') 
+    password =models.CharField(max_length=50 , default='null') 
 
